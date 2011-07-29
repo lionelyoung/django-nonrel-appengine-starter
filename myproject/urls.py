e6e88bc613d99a3a256ee56c7f30421f58c454c4
Lionel Young <lionelyoung@gmail.com>
2011-07-28 22:01:25 -0700

    Add url routing for registration

--- myproject/urls.py
+++ myproject/urls.py
@@ -6,6 +6,7 @@ handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
+    (r'^accounts/', include('registration.backends.default.urls')),
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     ('^$', 'django.views.generic.simple.direct_to_template',
      {'template': 'home.html'}),
