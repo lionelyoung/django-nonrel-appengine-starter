0e8bf11348a4f8bdee68d6b874b3082cf4a0e368
Lionel Young <lionelyoung@gmail.com>
2011-07-28 20:45:44 -0700

    Add example project

---
+++ myproject/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+admin.autodiscover()
+
+handler500 = 'djangotoolbox.errorviews.server_error'
+
+urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
+    ('^_ah/warmup$', 'djangoappengine.views.warmup'),
+    ('^$', 'django.views.generic.simple.direct_to_template',
+     {'template': 'home.html'}),
+)
